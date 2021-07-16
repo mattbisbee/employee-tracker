3246671c00c21e9a35c53a347403118100949daf
Biznaz <aldhelm7@gmail.com>
2021-07-16 11:38:58 -0700

    added info to db and server.js file

---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO employee (first_name, last_name)
+VALUES
+('Hal', 'Jordan'),
+('Scrooge','McDuck'),
+('Charles','Xavier'),
+('Peter','Parker'),
+('Bruce','Wayne'),
+('Jean','Grey'),
+('Jessica','Jones'),
+('Mary-Jane','Watson'),
+('Emma','Frost'),
+('Mina','Harker');
+
+INSERT INTO role (title, salary)
+VALUES
+('Salesperson','80000'),
+('Sales Lead','100000'),
+('Software Engineer','120000'),
+('Software Lead','150000'),
+('Accountant','125000'),
+('Finance Lead','160000'),
+('Lawyer','190000'),
+('Legal Team Lead','250000');
+
+INSERT INTO department (name)
+VALUES
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
