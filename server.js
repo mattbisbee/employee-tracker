e426c4aa194b3a65f190aa4f263d73282c374892
Biznaz <aldhelm7@gmail.com>
2021-07-16 11:42:11 -0700

    added a connection to db

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,24 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Biznaz0814!',
+    database: 'employee_db'
+  },
+  console.log('Connected to employee_db.')
+);
+
+db.query(`SELECT * FROM employee`, (err, rows) => {
+  console.log(rows);
+});
+
 
 // Default response for any other request (Not Found). Should be placed LAST!
 app.use((req, res) => {
